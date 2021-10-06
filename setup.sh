a6e78710c9b0b83c3c5924a6978f273305c3d948
jtgrassel <jtgrassel@gmail.com>
2021-10-06 10:32:38 -0700

    Adjustments for 40P on the cluster

--- setup.sh
+++ setup.sh
@@ -7,5 +7,12 @@
 curl -o MPEG7.zip https://dabi.temple.edu/external/shape/MPEG7/MPEG7dataset.zip
 unzip MPEG7.zip && mv original MPEG7 && rm MPEG7.zip
 
+#Remove unwanted files from MPEG7 download
+rm MPEG7/confusions.eps && rm MPEG7/confusions.fig && rm MPEG7/confusions.gif
+rm MPEG7/rat-09.gif && rm MPEG7/shapedata.eps && rm MPEG7/shapedata.fig && rm MPEG7/shapedata.gif
+
 # Create directory for generated images
 mkdir Output_Images
+mkdir Output_Images/Positive
+mkdir Output_Images/Negative
+mkdir Output_Images/White
